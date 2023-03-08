43b8f9ce9b8556fbbd8c8882aa4e06824f210ad9
unknown <Savioanjos2020@gmail.com>
2023-03-08 10:04:19 -0300

    Criando rota para deletar categorias

--- src/server.ts
+++ src/server.ts
@@ -58,6 +58,25 @@ app.get('/category', async () => {
   return { categories }
 })
 
+
+//Deletar categoria
+app.delete('/category', async (request) => {
+  const deleteCategorySchema = z.object({
+    id: z.string(),
+  })
+
+  const { id } = deleteCategorySchema.parse(request.body)
+
+
+  const deleteCategory = await prisma.category.delete({
+    where: {
+      id: id,
+    }
+  })
+
+  return { deleteCategory }
+})
+
 app.listen({
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
