1bf7bc240928e934b2afe013e71c3fb1a3d25d56
unknown <Savioanjos2020@gmail.com>
2023-03-08 20:30:11 -0300

    Finalizando API

--- src/server.ts
+++ src/server.ts
@@ -217,6 +217,7 @@ app.delete('/item', async (request, reply) => {
 
 })
 
+//--------------------------
 app.listen({
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
